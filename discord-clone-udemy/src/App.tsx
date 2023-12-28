fb7045387294dbc55e572b1c7eaa5f1ceb1d411e
Eiji Toriki <ageForEngi@outlook.jp>
2023-12-28 22:09:56 +0900

    受講開始

---
+++ discord-clone-udemy/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import Sidebar from './component/sidebar/Sidebar';
+
+function App() {
+  return (
+    <div>
+      {/* SideBar */}
+      <Sidebar />
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
